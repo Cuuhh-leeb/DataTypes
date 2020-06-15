d7eef444de741001ded50eb3c8fac00fa1de6dfd
Calieb Simpson <caliebsimpson@caliebs-mbp-2.attlocal.net>
2020-06-15 16:36:30 -0400

    DataTypes

---
+++ src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args){
+        boolean isSkyBlue = true;
+        byte myByte = 127;
+        char myChar = 'a';
+        short myShortVariable = -30000;
+        int myInteger = 90000;
+        float myFloat = 7.42f;
+        double myDouble = -90.28333;
+        long longVariable = 99999990;
+
+        System.out.println("My boolean variable :" + isSkyBlue);
+        System.out.println("My byte variable is equal to " + myByte);
+        System.out.println("My char variable is equal to " + myShortVariable);
+        System.out.println("My integer plus 1k is equal to " + (myInteger + 1000));
+    }
+}
